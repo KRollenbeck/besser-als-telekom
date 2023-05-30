4609f041efb26773f9a945357ef076f9d1a67d63
Konrad Rollenbeck <konrad.rollenbeck@gmail.com>
2023-05-31 00:05:18 +0200

    GUI Frames

--- GUI.py
+++ GUI.py
@@ -3,23 +3,31 @@ from tkinter.ttk import *
 
 class TopFrame(tk.Frame):
     def __init__(self, master):
-        super().__init__(master=master, height=200, bg="red")
+        super().__init__(master=master, height=20, bg="red")
         self.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
 
-class SideMenu:
+class SideMenu(tk.Frame):
     def __init__(self, master):
-        self.master = master
+        super().__init__(master=master, height=200,  width=200, bg="blue")
+        self.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
 
-class MainFrame:
+class MainFrame(tk.Frame):
     def __init__(self, master):
-        self.master = master
+        super().__init__(master=master, height=800, width=1200, bg="white")
+        self.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+class MiddleFrame(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master=master, height=1600)
+        self.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
 
 class Window:    
     def __init__(self):
         self.root = tk.Tk()
         self.top = TopFrame(self.root)
-        self.side = SideMenu(self.root)
-        self.main = MainFrame(self.root)
+        self.middle = MiddleFrame(self.root)
+        self.side = SideMenu(self.middle)
+        self.main = MainFrame(self.middle)
         self.root.mainloop()
 
 if __name__ == "__main__":
